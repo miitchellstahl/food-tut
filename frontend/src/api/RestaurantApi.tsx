a4bc0d65bc4d5d8fffb24a1493e79048c44218e7
Mitchell Stahl <mitchstahl11@gmail.com>
2024-05-13 13:33:17 -0400

    nyc additions

--- frontend/src/api/RestaurantApi.tsx
+++ frontend/src/api/RestaurantApi.tsx
@@ -1,5 +1,10 @@
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 export const useSearchRestaurants = (city?: string) => {
   const createSearchRequest = async () => {
     const response = await fetch(`${API_BASE_URL}/api/restaurant/search/:city`);
+
+    if (!response) {
+    }
   };
 };
